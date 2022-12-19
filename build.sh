481c9ca8f05882ab59ea5f7027d823b49a19cf30
SXionNial <104484446+SXionNial@users.noreply.github.com>
2022-12-19 12:58:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 
 set -o errexit  # exit on error
 
-heroku run pip install -r requirements.txt
+pip install -r requirements.txt
 
-heroku config:set DISABLE_COLLECTSTATIC=1
-heroku run python manage.py migrate
-heroku run 'bower install --config.interactive=false;grunt prep;python manage.py collectstatic --noinput'
-heroku config:unset DISABLE_COLLECTSTATIC
-heroku run python manage.py collectstatic
+python manage.py migrate
